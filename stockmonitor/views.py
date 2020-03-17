1bfe3c00046f10dff534046d2211afc5427354aa
leizhen <thunderlz@163.com>
2020-03-17 20:19:51 +0800

    views

--- stockmonitor/views.py
+++ stockmonitor/views.py
@@ -18,7 +18,9 @@ def stock(request):
      from stocksdaily sd left join stocklist sl on sd.ts_code = sl.ts_code 
      where sd.trade_date=(select trade_date from stocksdaily order by trade_date desc limit 1)''',con=engine)
     df_up=df[df['涨跌']>=0]
+    df_up.sort_values('涨跌',ascending=False,inplace=True)
     df_down=df[df['涨跌']<0]
+    df_down.sort_values('涨跌',ascending=True,inplace=True)
     stock={'stocks_up':df_up.iterrows(),'stocks_down':df_down.iterrows(),'stock_columns':df.columns,'stocks_up_count':df_up.shape[0],'stocks_down_count':df_down.shape[0]}
     datadic={'title':'股票信息','now':datetime.now().strftime('%y年%m月%d日%H时%M分%S秒'),'stocks':stock}
     return render(request,'stockmonitor/stock.html',context=datadic)
