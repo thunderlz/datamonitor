8fe5b1651f1b3d2242b1071b1b74ee4048f5b552
雷震 <leizhen@Leizs-MacBook-Pro-16.local>
2020-03-19 00:07:26 +0800

    settings

--- datamonitor/settings.py
+++ datamonitor/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-#     # '/var/www/static/',
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+]
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
