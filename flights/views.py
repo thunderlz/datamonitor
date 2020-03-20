84f3b503f1f5f3f8eeb0ccd06c11a48c4e5a2125
leizhen <thunderlz@163.com>
2020-03-20 15:28:56 +0800

    mac

--- flights/views.py
+++ flights/views.py
@@ -7,15 +7,22 @@ engine=create_engine('mysql+pymysql://root:751982leizhen@192.168.31.199/saflight
 # Create your views here.
 
 def todayflights(request):
-    today=datetime.now().strftime('%Y%m%d')
+    if request.method=='GET':
+        queryday=datetime.now().strftime('%Y%m%d')
+        depport=''
+        arrport=''
+        depdate=''
+    else:
+        queryday=request.POST.get('querydate')
+        depport=request.POST.get('depport')
+        arrport=request.POST.get('arrport')
+        depdate=request.POST.get('depdate')
     sql='''select airLine  ,arrDate ,arrPort ,arrTime ,arrivalTerminal ,codeShare ,codeShareInfo ,
     depDate ,depPort ,depTime ,departureTerminal ,flightNo ,meal ,plane ,rate 
-    ,timeDuringFlight ,lowprice ,querydate from flights where querydate like "{}%%"'''.format(today)
-    # sql='''select airLine  ,arrDate ,arrPort ,arrTime ,arrivalTerminal ,codeShare ,codeShareInfo ,
-    # depDate ,depPort ,depTime ,departureTerminal ,flightNo ,meal ,plane ,rate ,roundTripCabin 
-    # ,segInterval ,stopNameEn ,stopNameZh  ,stopNumber ,term ,timeDuringFlight ,timeDuringFlightEn  
-    # ,lowprice ,querydate from flights limit 200'''
+    ,timeDuringFlight ,lowprice ,querydate from flights 
+    where querydate like "{}%%" and depport like "%%{}%%" and arrport like "%%{}%%" and depdate like "%%{}%%" '''.format(queryday,depport,arrport,depdate)
+
     df=pd.read_sql(sql,con=engine)
-    flights={"flights":df.iterrows(),'flights_columns':df.columns,'flights_count':df.shape[0]}
+    flights={"flights":df.iterrows(),'flights_columns':df.columns,'flights_count':df.shape[0],'queryparam':{'queryday':queryday,'depport':depport,'arrport':arrport,'depdate':depdate}}
     datadic={'flights':flights}
     return render(request,'flights/todayflights.html',context=datadic)
