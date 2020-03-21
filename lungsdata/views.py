48cac140d5ab1498f56deb93943872200af08c2c
leizhen <thunderlz@163.com>
2020-03-21 22:09:13 +0800

    mac

--- lungsdata/views.py
+++ lungsdata/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from sqlalchemy import create_engine
 import pandas as pd
-engine=create_engine('mysql+pymysql://root:751982leizhen@192.168.31.199/lungsdata')
+engine=create_engine('mysql+pymysql://root:751982leizhen@192.168.31.200/lungsdata')
 # Create your views here.
 
 def lungsdata(request):
     sql='select * from jsondata order by time desc limit 1'
     df=pd.read_sql(sql,con=engine)
-    return HttpResponse(df.iat[0,0])
+    return HttpResponse(df.iat[0,1]+df.iat[0,0])
