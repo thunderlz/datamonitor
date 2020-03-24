4fe1f8f8742cad07812aae4e9149c22c661ac9b6
雷震 <leizhen@Leizs-MacBook-Pro-16.local>
2020-03-24 20:07:57 +0800

    mackbook

--- videomonitor/views.py
+++ videomonitor/views.py
@@ -5,9 +5,9 @@ from django.http import HttpResponse
 def videolog(request):
     try:
         with open('/mnt/data1/micam/xiaomi_camera_videos/log.txt','rt') as f:
-            # return HttpResponse(f.read().replace('\n','<br>'))
-            log=f.read().replace('\n','<br>')
-            datadic={'log':log}
-            return render(request,'videomonitor/log.html',context=datadic)
+            return HttpResponse(f.read().replace('\n','<br>'))
+            # log=f.read().replace('\n','<br>')
+            # datadic={'log':log}
+            # return render(request,'videomonitor/log.html',context=datadic)
     except:
         return HttpResponse('找不到这个文件。。。')
