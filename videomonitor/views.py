1417b4dab5c99214d9f4294ef1f3cff7aca3ee5d
雷震 <leizhen@Leizs-MacBook-Pro-16.local>
2020-03-24 20:22:55 +0800

    mackbook

--- videomonitor/views.py
+++ videomonitor/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def videolog(request):
     # try:
-        # with open('/mnt/data1/micam/xiaomi_camera_videos/log.txt','rt') as f:
-        with open('/Users/leizhen/log.txt','rt') as f:
+        with open('/mnt/data1/micam/xiaomi_camera_videos/log.txt','rt') as f:
+        # with open('/Users/leizhen/log.txt','rt') as f:
             # return HttpResponse(f.read().replace('\n','<br>'))
             log=f.read().replace('\n','<br>')
             datadic={'log':log}
